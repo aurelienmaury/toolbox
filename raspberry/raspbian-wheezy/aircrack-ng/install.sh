4cc1e64610c0fb9dfce35128e8dcd09940bdd277
Aurélien Maury <m4momo@gmail.com>
2013-11-19 00:55:24 +0100

    install aircrack

---
+++ raspberry/raspbian-wheezy/aircrack-ng/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apt-get -y install libssl-dev
+
+wget http://download.aircrack-ng.org/aircrack-ng-1.2-beta1.tar.gz
+
+tar xvzf aircrack-ng-1.2-beta1.tar.gz
+
+cd aircrack-ng-1.2-beta1
+
+make
+
+make install
